8aac7cce6b4ead4378512d711b70acabd2e42dfa
marian.galik <marian.galik@gmail.com>
2023-02-03 10:55:49 +0100

    pokracujem_v_zakladoch

--- zaklady.py
+++ zaklady.py
@@ -2,12 +2,18 @@
 # prezentovanych ako objekty
 import tensorflow as tf
 # dvojrozmerny tenzor
-x = tf.constant ([[1.,2.,2.],
+x = tf.constant ([[1.,2.,3.],
                  [4.,5.,6.]])
 print (x)
 print (x.shape) # informuje o velkosti tenzora po kazdej z jeho osi
 print (x.dtype) # informuje o type vsetkych prvkov v tenzore
 # TensorFlow implementuje standardne matematicke operacie na tenzoroch
-print (x+x) # scitanie 
-print (5*x) # nasobenie
+# scitanie 
+print (x+x) 
+# nasobenie
+print (5*x) 
+# Vrateny rozmer tenzora, zodpoveda vstupnemu rozmeru
+# Operacia vykonava standardne beznu maticovu transformaciu na 2-d vstupnych tenzoroch
+print (x @ tf.transpose(x)) 
 
+print (tf.nn.softmax(x, axis=-1))
